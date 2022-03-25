d63b5787e8748cebc0905c1172b35e90cb7a4064
Villager A <100194237+habataku@users.noreply.github.com>
2022-03-25 17:35:49 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://vio.gkpass.f5.si/bare/',
+    bare: 'https://ultra01.herokuapp.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
