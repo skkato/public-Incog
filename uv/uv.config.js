e8e6e9964533b18ac98540f260e1c837f873fcb1
Villager A <100194237+habataku@users.noreply.github.com>
2022-03-26 10:24:02 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.up.railway.app/bare/',
+    bare: 'https://ultra-virid.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
