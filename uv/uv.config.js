16c4a6116bd37b1640df7b83ad24a709af16d572
誠 <87258462+Taisei-dev@users.noreply.github.com>
2022-07-22 20:34:18 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://ultraviolet-node-nu.vercel.app/bare/',
+    bare: 'https://uvserver.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
